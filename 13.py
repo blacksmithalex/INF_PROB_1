756442145d56251ac2cfea9ba0223995a744722b
Алексей <alex@MacBook-Pro-Aleksej.local>
2025-03-03 18:16:11 +0300

    03mar25

---
+++ 13.py
@@ -0,0 +1,8 @@
+from ipaddress import *
+for mask in range(1, 32 + 1):
+    net1 = ip_network(f'84.77.47.132/{mask}', 0)
+    net2 =ip_network(f'84.77.48.132/{mask}', 0)
+    if net1 != net2:
+        print(mask, net1.netmask, f'{net1.netmask:b}')
+#3 слева байт будет иметь вид 11110000
+print('Result:', int('11110000', 2))
