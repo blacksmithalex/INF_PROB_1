f8bd35631813bacf9c92f55a584cf051d5ac1139
Алексей <alex@MacBook-Pro-Aleksej.local>
2025-11-04 19:10:04 +0300

    04nov25

--- 17.py
+++ 17.py
@@ -2,12 +2,12 @@ file = open('17.txt')
 a = [int(x) for x in file]
 file.close()
 
-m2 = 0
+m2 = -float('inf')
 for x in a:
-    if 10 <= x < 100 and x > m2:
+    if len(str(abs(x))) == 2 and x > m2:
         m2 = x
 
-count, sm = 0, 0
+count, sm = 0, -float('inf')
 for i in range(len(a) - 1):
     rule1 = a[i] >= 0 and len(str(a[i])) == 3
     rule2 = a[i + 1] >= 0 and len(str(a[i + 1])) == 3
