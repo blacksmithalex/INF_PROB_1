ceed4afbe984dc770bb90631e34ac92e9a59895f
Алексей <alex@MacBook-Pro-Aleksej.local>
2025-11-04 17:58:04 +0300

    04nov25

--- 12.py
+++
@@ -1,6 +0,0 @@
-for n in range(101, 1000):
-    s = '1' * n
-    while '111' in s:
-        s = s.replace('111', '22', 1)
-        s = s.replace('222', '11', 1)
-    print(f'Длина = {n}, кол-во единиц: {s.count('1')}')
